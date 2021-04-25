2aa3904f07cb6d6bc623cd8a40533e65f7c85418
chris <1553801752@qq.com>
2021-04-25 12:23:40 +0800

    contacts test ver2.0

--- app/src/main/java/com/example/contactstest/MainActivity.kt
+++ app/src/main/java/com/example/contactstest/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.contactstest
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val contactsList = ArrayList<String>()
+
+    private lateinit var adapter: ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //ListView初始化
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, contactsList)
+        contactsView.adapter = adapter
+
+        //READ_CONTACTS为危险权限，需要运行时权限处理流程
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_CONTACTS), 1)
+        } else {
+            readContacts()
+        }
+    }
+
+//    用户选择同意或不同意都会回调该函数，授权的结果在grantResults中
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    readContacts()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    private fun readContacts() {
+        //查询联系人数据
+        //cursor类
+        contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null)?.apply {
+            while (moveToNext()) {
+                //获取联系人姓名
+                val displayName = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+                //获取联系人手机号
+                val number = getString(getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER))
+                contactsList.add("$displayName\n$number")
+            }
+            adapter.notifyDataSetChanged()  //通知刷新一下ListView
+            close()
+        }
     }
 }
